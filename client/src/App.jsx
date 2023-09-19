6733cef54291f117fbba426adb7bc2b76bcb883f
lillitaz <lilli.weitzer@gmail.com>
2023-09-19 14:36:34 +0200

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import codennectLogo from './assets/dockerizer-logo.png'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <header className="App-header"/>
+            <img src={codennectLogo} className="codennect-logo" alt="logo" />
+            <p>
+                Codennect
+            </p>
+        </>
+  )
+}
+
+export default App;
